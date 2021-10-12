b563bf4fa2fea36e4fefb5b4ab0e6cff78e03d87
jaymesC <clementjaymes@gmail.com>
2021-10-12 21:16:06 +0100

    updated footer component

--- components/Footer.js
+++ components/Footer.js
@@ -7,15 +7,15 @@ function Footer() {
         <p>United States</p>
       </div>
       <div className="grid grid-cols-1">
-        <div>
+        <div className="flex justify-center items-center">
             <GlobeIcon className="h-5 text-green-700"/>Carbon neutral since 2007
             </div>
-      <div>
+      <div className="flex justify-center space-x-8 whitespace-nowrap md:justify-self-start">
           <p>Advertising</p>
           <p>Business</p>
           <p>How Search works</p>
       </div>
-      <div>
+      <div className="flex justify-center space-x-8 md:ml-auto">
           <p>Privacy</p>
           <p>Terms</p>
           <p>Settings</p>
