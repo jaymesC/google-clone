d4f91783c1a98f0c24856deaffd5ffa4f9fedae3
jaymesC <clementjaymes@gmail.com>
2021-10-12 20:48:28 +0100

    updated body section

--- pages/index.js
+++ pages/index.js
@@ -42,6 +42,12 @@ export default function Home() {
           <input type="text" className="focus:outline-none flex-grow" />
           <MicrophoneIcon className="h-5" />
         </div>
+
+        <div className="flex flex-col w-1/2 space-y-2 justify-center mt-8 sm:space-y-0 sm:flex-row sm:space-x-4">
+          <button className="btn">Google Search</button>
+
+          <button className="btn">I'm Feeling Lucky</button>
+        </div>
       </form>
 
       {/* footer */}
