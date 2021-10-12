6110f4c5c789290ca3225e7f1284ac73db6ab286
jaymesC <clementjaymes@gmail.com>
2021-10-12 20:53:24 +0100

    created footer component

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+import {GlobeIcon } from '@heroicons/react/solid'
+
+function Footer() {
+    return (
+        <footer>
+            <div></div>
+        </footer>
+    )
+}
+
+export default Footer
