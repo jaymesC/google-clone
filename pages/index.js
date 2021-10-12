c96c2ab64ff518faa3d90426d7b76715264b29a3
jaymesC <clementjaymes@gmail.com>
2021-10-12 21:37:11 +0100

    added reference to the input field

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import Head from "next/head";
 import Avartar from "../components/Avartar";
 import { MicrophoneIcon, ViewGridIcon } from "@heroicons/react/solid";
@@ -6,6 +7,15 @@ import Image from "next/image";
 import Footer from "../components/Footer";
 
 export default function Home() {
+  const searchInputRef = useRef(null);
+
+  const search = (e) => {
+    e.preventDefault();
+    const term = searchInputRef.current.value;
+
+    if(!term) return;
+  };
+
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <Head>
@@ -40,14 +50,22 @@ export default function Home() {
         />
         <div className="flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md rounded-full border border-gray-200 px-5 py-3 items-center sm:max-w-xl lg:max-w-2xl ">
           <SearchIcon className="h-5 mr-5 text-gray-500" />
-          <input type="text" className="focus:outline-none flex-grow" />
+          <input
+            ref={searchInputRef}
+            type="text"
+            className="focus:outline-none flex-grow"
+          />
           <MicrophoneIcon className="h-5" />
         </div>
 
         <div className="flex flex-col w-1/2 space-y-2 justify-center mt-8 sm:space-y-0 sm:flex-row sm:space-x-4">
-          <button className="btn">Google Search</button>
+          <button className="btn" onClick={search}>
+            Google Search
+          </button>
 
-          <button className="btn">I'm Feeling Lucky</button>
+          <button className="btn" onClick={search}>
+            I'm Feeling Lucky
+          </button>
         </div>
       </form>
 
