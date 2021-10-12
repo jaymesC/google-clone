78d15cbdd2aa2343da492a4ba85f806b2c8cc876
jaymesC <clementjaymes@gmail.com>
2021-10-12 21:49:10 +0100

    updated search page

--- pages/search.js
+++ pages/search.js
@@ -1,9 +1,16 @@
+import Head from "next/head";
+
 function Search() {
     return (
-        <div>
-            
-        </div>
-    )
+      <div>
+        <Head>
+          <title>Search Results</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        
+      </div>
+    );
 }
 
-export default Search
+export default Search;
