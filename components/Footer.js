1024a580fb779152f210b018039bad53d894c4c9
jaymesC <clementjaymes@gmail.com>
2021-10-12 21:10:17 +0100

    updated footer component

--- components/Footer.js
+++ components/Footer.js
@@ -6,8 +6,20 @@ function Footer() {
       <div className="px-8 py-3">
         <p>United States</p>
       </div>
+      <div className="grid grid-cols-1">
+        <div>
+            <GlobeIcon className="h-5 text-green-700"/>Carbon neutral since 2007
+            </div>
       <div>
-        <div><GlobeIcon className="h-5 text-green-700"/>Carbon neutral since 2007</div>
+          <p>Advertising</p>
+          <p>Business</p>
+          <p>How Search works</p>
+      </div>
+      <div>
+          <p>Privacy</p>
+          <p>Terms</p>
+          <p>Settings</p>
+      </div>
       </div>
     </footer>
   );
