21993a5d5cbbd63364af6f1a85eb91b2505f5265
jaymesC <clementjaymes@gmail.com>
2021-10-13 10:06:08 +0100

    chore(): updated Header Component

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,11 @@
 import Image from "next/image";
 import {useRouter} from "next/router";
+import {useRef} from "react"
 
 function Header() {
-    const router = useRouter(null)
+    const router = useRouter();
+    const searchInputRef = useRef(null)
+
     const homePage = () => {
         router.push('/')
     }
@@ -15,6 +18,9 @@ function Header() {
         className="cursor-pointer"
         onClick={homePage}
       />
+      <form className="flex px-6 py-3 ml-10 mr-5 border border-gray-200 rounded-full shadow-lg max-w-3xl items-center">
+          <input ref={searchInputRef} className="flex-grow w-full focus:outline-none" type="text" />
+      </form>
     </header>
   );
 }
