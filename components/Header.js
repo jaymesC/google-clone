339defbe25c9c6a55dc6b98e909b3a417d51212d
jaymesC <clementjaymes@gmail.com>
2021-10-13 10:32:18 +0100

    chore(): updated Header Component

--- components/Header.js
+++ components/Header.js
@@ -21,32 +21,34 @@ function Header() {
   };
 
   return (
-    <header>
-      <Image
-        src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
-        height={40}
-        width={120}
-        className="cursor-pointer"
-        onClick={homePage}
-      />
-      <form className="flex px-6 py-3 ml-10 mr-5 border border-gray-200 rounded-full shadow-lg max-w-3xl items-center">
-        <input
-          ref={searchInputRef}
-          className="flex-grow w-full focus:outline-none"
-          type="text"
+    <header className="sticky top-0 bg-white">
+      <div className="flex w-full p-6 items-center">
+        <Image
+          src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
+          height={40}
+          width={120}
+          className="cursor-pointer"
+          onClick={homePage}
         />
-        <XIcon
-          className="h-7 sm:mr-3 text-gray-500 cursor-pointer transition duration-100 transform hover:scale-125"
-          onClick={() => {
-            searchInputRef.current.value = "";
-          }}
-        />
-        <MicrophoneIcon className=" mr-3 h-6 hidden sm:inline-flex text-blue-500 border-l-2 pl-4 border-gray-300" />
-        <SearchIcon className=" h-6 text-blue-500 hidden sm:inline-flex" />
-        <button hidden type="submit" onClick={search}>
-          Search
-        </button>
-      </form>
+        <form className="flex flex-grow px-6 py-3 ml-10 mr-5 border border-gray-200 rounded-full shadow-lg max-w-3xl items-center">
+          <input
+            ref={searchInputRef}
+            className="flex-grow w-full focus:outline-none"
+            type="text"
+          />
+          <XIcon
+            className="h-7 sm:mr-3 text-gray-500 cursor-pointer transition duration-100 transform hover:scale-125"
+            onClick={() => {
+              searchInputRef.current.value = "";
+            }}
+          />
+          <MicrophoneIcon className=" mr-3 h-6 hidden sm:inline-flex text-blue-500 border-l-2 pl-4 border-gray-300" />
+          <SearchIcon className=" h-6 text-blue-500 hidden sm:inline-flex" />
+          <button hidden type="submit" onClick={search}>
+            Search
+          </button>
+        </form>
+      </div>
     </header>
   );
 }
