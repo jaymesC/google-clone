81a30c63a1005f1231d03c1cea65e194e27760c0
jaymesC <clementjaymes@gmail.com>
2021-10-14 10:22:44 +0100

    chore(): updated HeaderOption Component

---
+++ components/HeaderOption.js
@@ -0,0 +1,13 @@
+function Headeroption({ Icon, title, selected }) {
+  return (
+    <div
+      className={`flex items-center space-x-1 border-b-4 border-transparent hover:text-blue-500 hover:border-blue-500 pb-3 cursor-pointer ${
+        selected && "text-blue-500 border-blue-500"
+      }`}
+    >
+      <Icon />
+    </div>
+  );
+}
+
+export default Headeroption;
