8bfd58a684a2bae2a094f20f18ef2a3719119967
jaymesC <clementjaymes@gmail.com>
2021-10-14 10:09:09 +0100

    chore(): updated HeaderOptions Component

--- components/HeaderOptions.js
+++ components/HeaderOptions.js
@@ -1,7 +1,17 @@
+import HeaderOption from './HeaderOption';
+
 function Headeroptions() {
     return (
         <div>
-            
+
+        <div className="flex space-x-6">
+            <HeaderOption />
+        </div>
+
+        <div className="flex space-x-4">
+            <p className="link">Settings</p>
+            <p className="link">Tools</p>
+        </div>
         </div>
     )
 }
