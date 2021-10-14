75135ff97b01cc6ac3247cbc6b9fee80e6c933b8
jaymesC <clementjaymes@gmail.com>
2021-10-14 10:14:25 +0100

    chore(): updated HeaderOptions Component

--- components/HeaderOptions.js
+++ components/HeaderOptions.js
@@ -1,19 +1,31 @@
-import HeaderOption from './HeaderOption';
+import HeaderOption from "./HeaderOption";
+import {
+  DotsVerticalIcon,
+  MapIcon,
+  NewspaperIcon,
+  PhotographIcon,
+  PlayIcon,
+  SearchIcon,
+} from "@heroicons/react/outline";
 
 function Headeroptions() {
-    return (
-        <div>
+  return (
+    <div>
+      <div className="flex space-x-6">
+        <HeaderOption Icon={SearchIcon} title="All" selected/>
+        <HeaderOption Icon={PhotographIcon} title="Images" selected/>
+        <HeaderOption Icon={PlayIcon} title="Videos" selected/>
+        <HeaderOption Icon={NewspaperIcon} title="News" selected/>
+        <HeaderOption Icon={MapIcon} title="Maps" selected/>
+        <HeaderOption Icon={DotsVerticalIcon} title="More" selected/>
+      </div>
 
-        <div className="flex space-x-6">
-            <HeaderOption />
-        </div>
-
-        <div className="flex space-x-4">
-            <p className="link">Settings</p>
-            <p className="link">Tools</p>
-        </div>
-        </div>
-    )
+      <div className="flex space-x-4">
+        <p className="link">Settings</p>
+        <p className="link">Tools</p>
+      </div>
+    </div>
+  );
 }
 
 export default Headeroptions;
