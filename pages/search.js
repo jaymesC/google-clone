76dc3b8d2d479f56e42e875281f6aa387a32a497
jaymesC <clementjaymes@gmail.com>
2021-10-15 10:04:42 +0100

    chore(): updated search page

--- pages/search.js
+++ pages/search.js
@@ -1,24 +1,31 @@
 import Head from "next/head";
 import Header from "../components/Header";
+import { API_KEY, CONTEXT_KEY } from "../keys";
 
 function Search() {
-    return (
-      <div>
-        <Head>
-          <title>Search Results</title>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
+  return (
+    <div>
+      <Head>
+        <title>Search Results</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-        <Header/>
-      </div>
-    );
+      <Header />
+    </div>
+  );
 }
 
 export default Search;
 
-
 export async function getServerSideProps(context) {
-    const useDummyData = false;
+  const useDummyData = false;
 
-    const data = 
+  const data = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}`
+  );
+
+//   After server has rendered... Pass the results to the client...
+    return {
+        props
+    }
 }
