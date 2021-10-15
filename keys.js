eb3715b733d92c6b988be99431b9ed17ee0105b7
jaymesC <clementjaymes@gmail.com>
2021-10-15 09:54:53 +0100

    chore(): added many files

--- keys.js
+++ keys.js
@@ -1,3 +1,5 @@
-// API Key
+// API Key - https://developers.google.com/custom-search/v1/using_rest
+export const API_KEY = "AIzaSyCBubnfaiCoWhT1bGvvizldLhHD7HOpYyA";
 
-// Context key 
+// Context key - https://cse.google.com/cse/create/new
+export const CONTEXT_KEY = "cc05cd7af5ba078fc";
