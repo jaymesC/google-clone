ddc339f86b8cbf74024110db9b4c586810b2c639
jaymesC <clementjaymes@gmail.com>
2021-10-16 15:01:49 +0100

    chore(): updated SearchResults component

--- components/SearchResults.js
+++ components/SearchResults.js
@@ -6,9 +6,19 @@ function SearchResults({ results }) {
         {results.searchInformation.formattedSearchTime} seconds)
       </p>
 
-      {results.items?.map((result) => {
-          <div key={result.link}></div>
-      })}
+      {results.items?.map((result) => (
+        <div key={result.link} className="max-w-xl mb-8">
+          <div className="group">
+            <a href={result.link} className="text-sml">
+              {result.formattedUrl}
+            </a>
+            <a href={result.link}>
+                <h2 className="truncate text-xl text-blue-800 font-medium group-hover:underline">{result.title}</h2>
+            </a>
+          </div>
+          <p className="line-clamp-2">{result.snippet}</p>
+        </div>
+      ))}
     </div>
   );
 }
