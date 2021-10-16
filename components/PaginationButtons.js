ddbcfc9d5878ba0c0a482e1661be71cbbd221e40
jaymesC <clementjaymes@gmail.com>
2021-10-16 15:24:09 +0100

    chore(): updated PaginationButtons component

--- components/PaginationButtons.js
+++ components/PaginationButtons.js
@@ -9,13 +9,13 @@ function PaginationButtons() {
     const startIndex = Number(router.query.start) || 0;
 
     return (
-      <div>
+      <div className="flex justify-between max-w-lg text-blue-700 mb-10">
         {startIndex >= 10 && (
           <Link
             href={`/search?term=${router.query.term}&start=${startIndex - 10}`}
           >
-            <div>
-              <ChevronLeftIcon />
+            <div className="flex flex-grow flex-col items-center cursor-pointer hover:underline">
+              <ChevronLeftIcon className="h-5" />
               <p>Previous</p>
             </div>
           </Link>
@@ -24,8 +24,8 @@ function PaginationButtons() {
         <Link
           href={`/search?term=${router.query.term}&start=${startIndex + 10}`}
         >
-          <div>
-            <ChevronRightIcon />
+          <div className="flex flex-grow flex-col items-center cursor-pointer hover:underline">
+            <ChevronRightIcon className="h-5" />
             <p>Next</p>
           </div>
         </Link>
