d01c0e1d7378bb0c00d6bb621d4ff9f0d9a1d763
jaymesC <clementjaymes@gmail.com>
2021-10-16 14:19:39 +0100

    chore(): modified search page

--- pages/search.js
+++ pages/search.js
@@ -1,14 +1,17 @@
 import Head from "next/head";
 import Header from "../components/Header";
 import { API_KEY, CONTEXT_KEY } from "../keys";
-import {Response } from "../Response";
+import Response  from "../Response";
+import { useRouter } from "next/router";
 
-function Search({results}) {
-    console.log(results);
+function Search({ results }) {
+  const router = useRouter();
+
+  console.log(results);
   return (
     <div>
       <Head>
-        <title>Search Results</title>
+        <title>{router.query.term} - Google Search</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -20,16 +23,19 @@ function Search({results}) {
 export default Search;
 
 export async function getServerSideProps(context) {
-  const useDummyData = false;
+  const useDummyData = true;
+  const startIndex = context.query.start || "0";
 
-  const data =useDummyData? Response : await fetch(
-    `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}`
-  ).then((response) => response.json());
+  const data = useDummyData
+    ? Response
+    : await fetch(
+        `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}&start=${startIndex}`
+      ).then((response) => response.json());
 
-//   After server has rendered... Pass the results to the client...
-    return {
-        props: {
-            results: data,
-        }
-    }
+  //   After server has rendered... Pass the results to the client...
+  return {
+    props: {
+      results: data,
+    },
+  };
 }
