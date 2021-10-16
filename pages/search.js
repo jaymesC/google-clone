fab5fac015090752424ad0e57425f6b31e314c70
jaymesC <clementjaymes@gmail.com>
2021-10-16 13:54:33 +0100

    chore(): updated search page

--- pages/search.js
+++ pages/search.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Header from "../components/Header";
 import { API_KEY, CONTEXT_KEY } from "../keys";
+import {Response } from "../Response";
 
 function Search({results}) {
     console.log(results);
@@ -21,7 +22,7 @@ export default Search;
 export async function getServerSideProps(context) {
   const useDummyData = false;
 
-  const data = await fetch(
+  const data =useDummyData? Response : await fetch(
     `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}`
   ).then((response) => response.json());
 
