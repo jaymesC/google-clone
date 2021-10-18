1684e47ab3a38aa1d62d344a1d14c605c74d0c75
JAYMES! <32098175+jaymesC@users.noreply.github.com>
2021-10-18 10:50:09 +0100

    deleted

--- keys.js
+++
@@ -1,5 +0,0 @@
-// API Key - https://developers.google.com/custom-search/v1/using_rest
-export const API_KEY = "AIzaSyCBubnfaiCoWhT1bGvvizldLhHD7HOpYyA";
-
-// Context key - https://cse.google.com/cse/create/new
-export const CONTEXT_KEY = "cc05cd7af5ba078fc";
